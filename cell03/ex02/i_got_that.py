a4ed77aa33af75b56a31f3abfc3b189e870bc394
ssangmue <67030348@kmitl.ac.th>
2024-11-20 17:08:29 +0000

    02

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,11 @@
+print(input("What you gotta say?"))
+while True:
+    user_input = input("I got that! Anything else?: ")
+    
+    
+    if user_input == "STOP":
+        print("Goodbye!")
+        break  
+    
+   
+    print(f" {user_input}")
