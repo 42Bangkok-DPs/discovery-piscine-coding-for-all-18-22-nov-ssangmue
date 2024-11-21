e49696b94be2fadb0f3dee86f1720d5c3beb1abe
ssangmue <67030348@kmitl.ac.th>
2024-11-21 04:45:53 +0000

    ghhyt

--- cell05/ex01/ play_with_arrays.py
+++ cell05/ex01/ play_with_arrays.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-# Original array
+
 original_array = [2, 8, 9, 48, 8, 22, -12, 2]
 
-# New array where each element is increased by 2
+
 new_array = [x + 2 for x in original_array]
 
-# Print the original and new arrays
+
 print(f"Original array: {original_array}")
 print(f"New array: {new_array}")
