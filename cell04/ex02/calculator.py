7988ff3bb2d360934467d41ce8c8bc6f4e914169
ssangmue <67030348@kmitl.ac.th>
2024-11-21 03:29:31 +0000

    2222

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python3
+
+
 num1 = float(input("Give me the first number: "))
 num2 = float(input("Give me the second number: "))
 
-# Perform arithmetic operations
+
+print("Thank you!")
+
+
 addition = num1 + num2
 subtraction = num1 - num2
 multiplication = num1 * num2
-# To avoid division by zero error, check if num2 is not zero
+
 if num2 != 0:
     division = num1 / num2
 else:
     division = "undefined (division by zero)"
 
-# Display the results
-print(f"The sum of {num1} and {num2} is: {addition}")
-print(f"The difference between {num1} and {num2} is: {subtraction}")
-print(f"The product of {num1} and {num2} is: {multiplication}")
-print(f"The division of {num1} by {num2} is: {division}")
+
+print(f"{num1} + {num2} = {addition}")
+print(f"{num1} - {num2} = {subtraction}")
+print(f"{num1} / {num2} = {division}")
+print(f"{num1} * {num2} = {multiplication}")
