907854f1c617c8608be15d1064eb3e71b48ec823
ssangmue <67030348@kmitl.ac.th>
2024-11-21 03:27:08 +0000

    2222

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,4 +1,4 @@
-age = int(input("Enter your age: "))
+age = int(input("Please tell me your age: "))
 
 
 age_in_10 = age + 10
@@ -6,6 +6,6 @@ age_in_20 = age + 20
 age_in_30 = age + 30
 
 
-print(f"In 10 years, you will be {age_in_10} years old.")
-print(f"In 20 years, you will be {age_in_20} years old.")
-print(f"In 30 years, you will be {age_in_30} years old.")
+print(f"In 10 years, you'll be  {age_in_10} years old.")
+print(f"In 20 years, you'll be {age_in_20} years old.")
+print(f"In 30 years, you'll be {age_in_30} years old.")
