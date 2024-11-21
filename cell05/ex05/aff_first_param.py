01f4557c974ba0f25002dae7e71fc7ff69f9e85a
ssangmue <67030348@kmitl.ac.th>
2024-11-21 05:07:12 +0000

    5555555

--- cell05/ex05/aff_first_param.py
+++ cell05/ex05/aff_first_param.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+
 import sys
 
-# Check if there are any parameters passed
+
 if len(sys.argv) < 2:
     print("none")
 else:
-    # Print the first parameter (excluding the script name)
+    
     print(sys.argv[1])
